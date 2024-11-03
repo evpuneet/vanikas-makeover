8e4a5a4b5db91e2a04a88e6975f89442e7d49581
Puneet <puneetkfl147@gmail.com>
2024-11-03 16:43:22 +0530

    10th commit

--- src/app/components/HeroSlider.jsx
+++ src/app/components/HeroSlider.jsx
@@ -1,78 +1,79 @@
 "use client"
 
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import { IoLocation } from "react-icons/io5";
-
-// Import Swiper styles
+import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
-
-
-// import required modules
 import { Autoplay, Scrollbar } from 'swiper/modules';
 
 export default function HeroSlider() {
   return (
     <>
-        <div className='relative'>
-            <Swiper
-            scrollbar={{
-                hide: true,
-            }}
-            autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
-              }}
-            modules={[Autoplay, Scrollbar]}
-            className="mySwiper"
-            >
-            <SwiperSlide>
-                <div className='w-[100vw] h-[100vh] grid lg:grid-cols-2 grid-cols-1 items-center lg:ps-[70px] ps-3 text-white lg:pt-0 pt-[150px]'>
-                    <div>
-                      <h2 className='font-Forum text-[40px] lg:mb-[20px]'>Beauty Salon of Jodhpur</h2>
-                      <p className='lg:mb-[50px] mb-[20px] md:text-[16px] text-[14px]'>
-                        At Vanikka's, we believe in the harmony of beauty and excellence.
-                        Our mission is to provide top-notch beauty services using the
-                        finest premium products. We strive to create an oasis of
-                        serenity where you can relax, rejuvenate, and leave looking and
-                        feeling your best.
-                      </p>
-                      <div className='flex gap-2'>
-                        <button className='bg-white text-primary text-[16px] lg:px-4 px-2 lg:py-2 py-1 rounded-lg' >Book Appointment</button>
-                        <button className='text-[16px] lg:px-4 px-2 lg:py-2 py-1 rounded-lg border flex items-center gap-2 hover:shadow'>Salon Locator <IoLocation  className='text-[16px]'/></button>
-                      </div>
-                    </div>
-                    <div className='ps-[40px] md:ps-0]'>
-                      <img className='lg:w-[600] w-[300] ' src="https://naturals.in/assets/banner_girl.png" alt="" />
-                    </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className='w-[100%] h-[100vh] md:bg-banner-2 bg-banner-m-2 bg-no-repeat bg-cover '>
-                </div>
-            </SwiperSlide>
-            </Swiper>
-        </div>
-        <div className='relative h-[200px]'>
-        <div className='bg-white rounded-[80px_80px_0_0] lg:py-12 lg:px-[200px] absolute top-[80px] w-[100%] -translate-y-[60%] z-[10] grid lg:grid-cols-2 grid-cols-1'>
-              <div className=''></div>
-              <div className='lg:text-right text-center text-primary'>
-                <h4 className='text-[40px] font-Forum '>Why Choose Us?</h4>
-                <p className='lg:text-[16px] text-[14px]'>
-                  With 24 years of experience, Vanikka's offers the benefit of
-                  over two decades of expertise in the beauty industry. Our
-                  personalized services are tailored to meet your unique needs
-                  and preferences, ensuring that you receive the care and
-                  attention you deserve. We are committed not to compromised
-                  quality, using only the highest quality premium products to
-                  provide exceptional care.
+      <div className='relative'>
+        <Swiper
+          scrollbar={{ hide: true }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Scrollbar]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className='w-[100vw] h-[100vh] grid lg:grid-cols-2 grid-cols-1 items-center lg:ps-[70px] ps-3 text-white lg:pt-0 pt-[150px]'>
+              <div>
+                <h2 className='font-Forum text-[40px] lg:mb-[20px]'>Beauty Salon of Jodhpur</h2>
+                <p className='lg:mb-[50px] mb-[20px] md:text-[16px] text-[14px]'>
+                  At Vanikka&apos;s, we believe in the harmony of beauty and excellence.
+                  Our mission is to provide top-notch beauty services using the
+                  finest premium products. We strive to create an oasis of
+                  serenity where you can relax, rejuvenate, and leave looking and
+                  feeling your best.
                 </p>
+                <div className='flex gap-2'>
+                  <button className='bg-white text-primary text-[16px] lg:px-4 px-2 lg:py-2 py-1 rounded-lg'>
+                    Book Appointment
+                  </button>
+                  <button className='text-[16px] lg:px-4 px-2 lg:py-2 py-1 rounded-lg border flex items-center gap-2 hover:shadow'>
+                    Salon Locator <IoLocation className='text-[16px]' />
+                  </button>
+                </div>
               </div>
+              <div className='ps-[40px] md:ps-0'>
+                <Image 
+                  src="https://naturals.in/assets/banner_girl.png" 
+                  alt="A woman at the salon" // Provide descriptive alt text
+                  width={600} // Set appropriate width
+                  height={400} // Set appropriate height
+                  className='w-[100%] h-auto' // Use responsive width
+                />
+              </div>
+          </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='w-[100%] h-[100vh] md:bg-banner-2 bg-banner-m-2 bg-no-repeat bg-cover '></div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div className='relative h-[200px]'>
+        <div className='bg-white rounded-[80px_80px_0_0] lg:py-12 lg:px-[200px] absolute top-[80px] w-[100%] -translate-y-[60%] z-[10] grid lg:grid-cols-2 grid-cols-1'>
+          <div className=''></div>
+          <div className='lg:text-right text-center text-primary'>
+            <h4 className='text-[40px] font-Forum '>Why Choose Us?</h4>
+            <p className='lg:text-[16px] text-[14px]'>
+              With 24 years of experience, Vanikka&apos;s offers the benefit of
+              over two decades of expertise in the beauty industry. Our
+              personalized services are tailored to meet your unique needs
+              and preferences, ensuring that you receive the care and
+              attention you deserve. We are committed not to compromise
+              quality, using only the highest quality premium products to
+              provide exceptional care.
+            </p>
+          </div>
         </div>
-        </div>
+      </div>
     </>
   )
 }
--- src/app/components/common/Header.jsx
+++ src/app/components/common/Header.jsx
@@ -1,50 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 export default function Header() {
-
     const menus = [
         {
-          id: "Our Services",
+          id: "our-services", // Changed to lowercase for URL-friendly IDs
           label: "Our Services",
         },
         {
-          id: "Location",
+          id: "location",
           label: "Location",
         },
         {
-          id: "Bridal",
+          id: "bridal",
           label: "Bridal",
         },
         {
-            id: "Pricing",
+            id: "pricing",
             label: "Pricing",
         },
         {
-          id: "About Us",
+          id: "about-us",
           label: "About Us",
         },
         {
-            id: "Blog",
+            id: "blog",
             label: "Blog",
         }
-      ];
+    ];
 
-  return (
-    <>
+    return (
         <header className='px-14 py-8 flex items-center lg:justify-between justify-center text-white absolute z-[99] w-[100%]'>
             <div>
-                <img src="/vanikasLogo.svg" className='md:w-[250px] w-[200px]' alt="Vanikas Logo" />
+                <Image 
+                    src="/vanikasLogo.svg" 
+                    alt="Vanikas Logo" 
+                    className='md:w-[250px] w-[200px]' 
+                    width={250} // Adjust width as needed
+                    height={100} // Adjust height as needed to maintain aspect ratio
+                />
             </div>
             <div>
                 <ul className='lg:flex hidden gap-5'>
-                    {menus.map((item)=>{
-                        return(
-                            <li key={item.key} className='text-[16px] cursor-pointer hover:drop-shadow-white-glow hover:font-semibold text-center transition-all min-w-[100px]'>{item.label}</li>
-                        )
+                    {menus.map((item) => {
+                        return (
+                            <li key={item.id} className='text-[16px] cursor-pointer hover:drop-shadow-white-glow hover:font-semibold text-center transition-all min-w-[100px]'>
+                                {item.label}
+                            </li>
+                        );
                     })}
                 </ul>
             </div>
         </header>
-    </>
-  )
+    );
 }
