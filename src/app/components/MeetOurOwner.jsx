63f7d2f78d2c3b9888a8461da14018db42e53f8c
Puneet <puneetkfl147@gmail.com>
2024-11-03 21:11:33 +0530

    17th commit

--- src/app/components/MeetOurOwner.jsx
+++ src/app/components/MeetOurOwner.jsx
@@ -14,7 +14,7 @@ export default function MeetOurOwner() {
                 </div>
                 <div className='text-primary text-center mb-[10px]'>
                     <h2 className='text-[40px] mb-[20px]'>Yogita Joshi</h2>
-                    <p className='text-[16px] mb-[20px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                    <p className='text-[16px] mb-[20px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                     <div>
                         <h4 className='mb-[10px]'>Follow Us On</h4>
                         <div className='flex justify-center gap-[30px]'>
