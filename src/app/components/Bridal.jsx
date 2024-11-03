0fe97f7e6736be01ca951fa0532576e7a5e82524
Puneet <puneetkfl147@gmail.com>
2024-11-03 16:18:22 +0530

    5th commit

--- src/app/components/Bridal.jsx
+++ src/app/components/Bridal.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Bridal() {
@@ -8,7 +9,7 @@ export default function Bridal() {
                 <div className='px-[130px] text-primary'>
                     <h4 className='text-[40px] font-Forum '>Why Choose Us?</h4>
                     <p className='text-[16px] mb-[10px]'>
-                    With 24 years of experience, Vanikka's offers the benefit of
+                    With 24 years of experience, Vanikka&apos;s offers the benefit of
                     over two decades of expertise in the beauty industry. Our
                     personalized services are tailored to meet your unique needs
                     and preferences, ensuring that you receive the care and
@@ -19,7 +20,7 @@ export default function Bridal() {
                     <button tel="54651635165" className='text-nowrap bg-primary rounded-[5px] text-white px-4 py-2 text-[14px] font-semibold'>Book Now</button>
                 </div>
                 <div className='relative z-[96] h-full'>
-                    <img src="/assets/bridal_img.png" className='absolute w-[700px] object-cover bottom-0 right-0' alt="" />
+                    <Image src="/assets/bridal_img.png" className='absolute w-[700px] object-cover bottom-0 right-0' width={700} height={100} alt="" />
                 </div>
             </div>
         </section>
@@ -28,7 +29,7 @@ export default function Bridal() {
                 <div className='text-primary'>
                     <h4 className='text-[40px] font-Forum '>Why Choose Us?</h4>
                     <p className='text-[16px] mb-[10px]'>
-                    With 24 years of experience, Vanikka's offers the benefit of
+                    With 24 years of experience, Vanikka&apos;s offers the benefit of
                     over two decades of expertise in the beauty industry. Our
                     personalized services are tailored to meet your unique needs
                     and preferences, ensuring that you receive the care and
